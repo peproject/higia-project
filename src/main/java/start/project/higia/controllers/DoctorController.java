6564f34bf6a24a64a48c01f281ed3a4f55ab77aa
Guilherme81 <alanguilherme0707@gmail.com>
2022-06-05 19:03:27 -0300

    :sparkles: feat: list route to doctor

--- src/main/java/start/project/higia/controllers/DoctorController.java
+++ src/main/java/start/project/higia/controllers/DoctorController.java
@@ -2,6 +2,7 @@ package start.project.higia.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,14 +16,20 @@ public class DoctorController {
 	@Autowired
 	private DoctorServices services;
 	
+	@GetMapping("/doctor")
+	public String index(Doctor doctor, Model model) {
+		model.addAttribute("doctor", this.services.index(doctor));
+		return "index";
+	}
+	
 	//Rota para tela de cadastro 
-	@GetMapping("doctor_registration")
+	@GetMapping("/doctor_registration")
 	public String registration() {
 		return "index";
 	}
 	
 	//Rota para salvar o doutor no banco de dados
-	@PostMapping("doctor")
+	@PostMapping("/doctor")
 	public String create(Doctor doctor) {
 		this.services.create(doctor);
 		return "index";
