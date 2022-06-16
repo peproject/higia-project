f55e4f10ab5218e9ba5bddaa619d7186e930fb2b
Nickolas Flamel <devflamel2022@gmail.com>
2022-06-16 19:57:00 -0300

    :sparkles: feat: Chart implemantation

--- src/main/java/start/project/higia/controllers/ChartController.java
+++ src/main/java/start/project/higia/controllers/ChartController.java
@@ -1,9 +1,12 @@
 package start.project.higia.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import start.project.higia.models.Chart;
@@ -14,21 +17,36 @@ public class ChartController {
 
 	@Autowired
 	private ChartServices services;
-	
+
 	@GetMapping("/chart")
 	public String index(Chart chart, Model model) {
 		model.addAttribute("chart", this.services.index(chart));
 		return "index";
 	}
-	
+
 	@GetMapping("chart_registration")
 	public String registration() {
-		return "index";
+		return "register/chart";
 	}
-	
+
 	@PostMapping("/chart")
 	public String create(Chart chart) {
 		this.services.create(chart);
 		return "index";
+	}
+
+	//rota para edição de chart
+	@GetMapping("/edit_chart/{id}")
+	public String edit_chart(@PathVariable Long id, Model model) {
+		Optional<Chart> chart = this.services.editById(id);
+		model.addAttribute("chart", chart);
+		return "edit/chart";
+	}
+
+	//rota para exclusão do chart
+	@GetMapping("/delete_chart")
+	public String delete_chart(Long id) {
+		services.deleteById(id);
+		return "redirect:/chart";
 	}
 }
--- src/main/java/start/project/higia/services/ChartServices.java
+++ src/main/java/start/project/higia/services/ChartServices.java
@@ -1,6 +1,7 @@
 package start.project.higia.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,16 +11,27 @@ import start.project.higia.repositories.ChartRepository;
 
 @Component
 public class ChartServices {
-	
+
 	@Autowired
 	private ChartRepository repository;
-	
+
 	public Chart create(Chart chart) {
 		return repository.save(chart);
 	}
-	
+
 	public List<Chart> index(Chart chart) {
 		return repository.findAll();
+	}
+
+	//serviço para editar o chart
+	public Optional<Chart> editById(Long id) {
+		return repository.findById(id);
+	}
+
+	//serviço para deletar o chart
+	public String deleteById(Long id) {
+		repository.deleteById(id);
+		return "";
 	}
 
 }
