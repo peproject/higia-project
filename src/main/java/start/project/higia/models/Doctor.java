d6111b678e2899783238f9c76369240e296c6eaa
Guilherme81 <alanguilherme0707@gmail.com>
2022-06-20 22:34:19 -0300

    :recycle: refactor: refactoring the model doctor

--- src/main/java/start/project/higia/models/Doctor.java
+++ src/main/java/start/project/higia/models/Doctor.java
@@ -7,6 +7,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,6 +37,7 @@ public class Doctor {
 	private String password;
 	
 	@Enumerated(EnumType.STRING)
+	@NotNull
 	private Roles role;
 	
 	public Roles getRole() {
