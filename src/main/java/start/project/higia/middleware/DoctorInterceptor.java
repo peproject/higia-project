a083f806b1d72894a83cbf6c795a789f43b5b533
Gabriel Mariz <gabriellucena997@outlook.com>
2022-07-01 22:38:19 -0300

    :recycle: refactor: interception

--- src/main/java/start/project/higia/middleware/DoctorInterceptor.java
+++ src/main/java/start/project/higia/middleware/DoctorInterceptor.java
@@ -9,27 +9,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class DoctorInterceptor implements HandlerInterceptor {
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		if (request.getSession().getAttribute("loggedDoctor") == null) {
+		if (request.getSession().getAttribute("doctor") == null) {
 			request.getRequestDispatcher("/doctor/login").forward(request, response);
 			return false;
 		} else {
 			return true;
 		}
 	}
-	
+
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 	       HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
-	
+
 	@Override
 	public void afterCompletion(
-	  HttpServletRequest request, 
+	  HttpServletRequest request,
 	  HttpServletResponse response,
 	  Object handler, Exception ex) throws Exception {
 	   HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
