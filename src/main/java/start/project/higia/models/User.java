8940ddfd638978353ebca6aa148eb6e19915ee32
Gabriel Mariz <gabriellucena997@outlook.com>
2022-07-04 15:11:21 -0300

    :bug: fix: date format

--- src/main/java/start/project/higia/models/User.java
+++ src/main/java/start/project/higia/models/User.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -41,6 +43,7 @@ public class User {
     private String password;
 
     @Column(nullable = false)
+		@DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
 
     @Column(nullable = false)
