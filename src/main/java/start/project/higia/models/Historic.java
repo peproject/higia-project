424ea63ba548750e36accad910c860dd5dd70761
Cauã Ricardo <cauakika@gmail.com>
2022-07-13 21:22:07 -0300

    :sparkles: feat: implementation of history

---
+++ src/main/java/start/project/higia/models/Historic.java
@@ -0,0 +1,45 @@
+package start.project.higia.models;
+
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Historic {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@OneToMany(cascade = CascadeType.ALL , mappedBy = "historic")
+	private List<Evolution> evolutions;
+	
+	@ManyToOne
+	private Doctor doctor;
+	
+	@ManyToOne
+	private User user;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedAt;
+
+	public List<Evolution> getEvolutions() {
+		return evolutions;
+	}
+
+	public void setEvolutions(List<Evolution> evolutions) {
+		this.evolutions = evolutions;
+	}
+
+
+}
