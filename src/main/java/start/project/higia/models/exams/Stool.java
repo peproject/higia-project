ec237be796fb9bf9d86e025bed4fcfe0041523ed
Cauã Ricardo <cauakika@gmail.com>
2022-07-15 20:23:54 -0300

    :recycle: refactor: Stool exam

--- src/main/java/start/project/higia/models/exams/Stool.java
+++ src/main/java/start/project/higia/models/exams/Stool.java
@@ -1,14 +1,10 @@
 package start.project.higia.models.exams;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.lang.Nullable;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
