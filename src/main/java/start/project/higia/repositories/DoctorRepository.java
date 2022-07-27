0078cb1be8c51a55faf09f64b8afabc44ec2b5c5
Gabriel Mariz <gabriellucena997@outlook.com>
2022-07-26 22:50:47 -0300

    :boom: fix: fix

--- src/main/java/start/project/higia/repositories/DoctorRepository.java
+++ src/main/java/start/project/higia/repositories/DoctorRepository.java
@@ -1,13 +1,7 @@
 package start.project.higia.repositories;
 
-<<<<<<< HEAD
-import java.util.Optional;
-=======
->>>>>>> 0c3ed74a1aa88e265d572c8ff553a6b49d2cc1b8
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import start.project.higia.models.Doctor;
@@ -18,10 +12,4 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 	public Doctor findByEmailAndPassword(String email, String password);
 	public Doctor findByEmail(String email);
 	
-<<<<<<< HEAD
-	@Query("select u from Doctor u where u.email = :email")
-	Optional<Doctor> findBEmail(@Param("email") String email);
-	
-=======
->>>>>>> 0c3ed74a1aa88e265d572c8ff553a6b49d2cc1b8
 }
