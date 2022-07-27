e1e5f251f0b05f0c973730e7e2ba4c07e0b00a11
Guilherme81 <alanguilherme0707@gmail.com>
2022-07-26 22:34:25 -0300

    :sparkles: feat: Search by email

--- src/main/java/start/project/higia/repositories/DoctorRepository.java
+++ src/main/java/start/project/higia/repositories/DoctorRepository.java
@@ -1,6 +1,10 @@
 package start.project.higia.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import start.project.higia.models.Doctor;
@@ -11,4 +15,8 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 	public Doctor findByEmailAndPassword(String email, String password);
 
 	public Doctor findByEmail(String email);
+	
+	@Query("select u from Doctor u where u.email = :email")
+	Optional<Doctor> findBEmail(@Param("email") String email);
+	
 }
