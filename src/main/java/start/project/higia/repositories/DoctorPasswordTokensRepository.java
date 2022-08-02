e8fc6b83b6caaa3c4bd5e82e467a3af14043146b
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-01 21:14:46 -0300

    :sparkles: feat: doctor token repository

--- src/main/java/start/project/higia/repositories/DoctorPasswordTokensRepository.java
+++ src/main/java/start/project/higia/repositories/DoctorPasswordTokensRepository.java
@@ -1,11 +1,24 @@
 package start.project.higia.repositories;
 
+import java.util.Date;
+import java.util.stream.Stream;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import start.project.higia.models.Doctor;
 import start.project.higia.models.DoctorPasswordTokens;
 
 @Component
 public interface DoctorPasswordTokensRepository extends JpaRepository<DoctorPasswordTokens, Long> {
+	
+	DoctorPasswordTokens findByToken(String token);
 
+	DoctorPasswordTokens findByDoctor(Doctor doctor);
+
+    Stream<DoctorPasswordTokens> findAllByExpiryDateLessThan(Date now);
+
+    void deleteByExpiryDateLessThan(Date now);
+
+	
 }
