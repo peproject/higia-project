10b1b99b3bbb4ce8ea2579439045082e1bc84c57
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-01 21:20:41 -0300

    :boom: fix: changing the logic of the doctor tokens model

--- src/main/java/start/project/higia/models/DoctorPasswordTokens.java
+++ src/main/java/start/project/higia/models/DoctorPasswordTokens.java
@@ -1,12 +1,15 @@
 package start.project.higia.models;
 
-import javax.persistence.Column;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,17 +20,101 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class DoctorPasswordTokens {
- 
+
+	private static final int EXPIRATION = 60 * 24;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String tokens;
-	@Column(nullable = false, length = 2)
-	private Integer used;
-	
-	@ManyToOne
-	@NotNull
+	private Long id;
+
+	private String token;
+
+	@OneToOne(targetEntity = Doctor.class, fetch = FetchType.EAGER)
+	@JoinColumn(nullable = false, name = "user_id")
 	private Doctor doctor;
+
+	private Date expiryDate;
+
+	public DoctorPasswordTokens(final String token) {
+        super();
+
+        this.token = token;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+	public DoctorPasswordTokens(final String token, final Doctor doctor) {
+        super();
+
+        this.token = token;
+        this.doctor = doctor;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+}
 	
-	
+    private Date calculateExpiryDate(final int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public void updateToken(final String token) {
+        this.token = token;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    //
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getExpiryDate() == null) ? 0 : getExpiryDate().hashCode());
+        result = prime * result + ((getToken() == null) ? 0 : getToken().hashCode());
+        result = prime * result + ((getDoctor() == null) ? 0 : getDoctor().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DoctorPasswordTokens other = (DoctorPasswordTokens) obj;
+        if (getExpiryDate() == null) {
+            if (other.getExpiryDate() != null) {
+                return false;
+            }
+        } else if (!getExpiryDate().equals(other.getExpiryDate())) {
+            return false;
+        }
+        if (getToken() == null) {
+            if (other.getToken() != null) {
+                return false;
+            }
+        } else if (!getToken().equals(other.getToken())) {
+            return false;
+        }
+        if (getDoctor() == null) {
+            if (other.getDoctor() != null) {
+                return false;
+            }
+        } else if (!getDoctor().equals(other.getDoctor())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Token [String=").append(token).append("]").append("[Expires").append(expiryDate).append("]");
+        return builder.toString();
+
+    }
 }
