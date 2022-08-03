15bfa219354ce8a5d10a59706d359268b84e5d8b
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-03 19:49:08 -0300

    :boom: fix: improving email search

--- src/main/java/start/project/higia/repositories/DoctorRepository.java
+++ src/main/java/start/project/higia/repositories/DoctorRepository.java
@@ -1,17 +1,22 @@
 package start.project.higia.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import start.project.higia.models.Doctor;
 
 @Component
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+	
+	@Query("SELECT u From Doctor u WHERE u.email LIKE :email")
+	Doctor findEmail(@Param("email") String  email);
+	
+	public Optional<Doctor> findByEmail(String email);
 
-	public Doctor findByEmailAndPassword(String email, String password);
-	
-	public Doctor findByEmail(String email);
-	
 	
 }
