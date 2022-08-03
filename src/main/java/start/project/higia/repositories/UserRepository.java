0d9295ca253ad4af734c020384ccb7ebf270745c
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-03 19:47:50 -0300

    :sparkles: feat: implementing search by email

--- src/main/java/start/project/higia/repositories/UserRepository.java
+++ src/main/java/start/project/higia/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package start.project.higia.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +10,5 @@ import start.project.higia.models.User;
 @Component
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	public User findByEmailAndPassword(String email, String password);
+	public Optional<User> findByEmail(String email);
 }
