e77cc4e0a6648440b3e5463984180d902aa2d997
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-06 19:36:34 -0300

    :sparkles: feat: copy text

---
+++ src/main/resources/static/scripts/copy_text.js
@@ -0,0 +1,11 @@
+function copy(name) {
+  /* Get the text field */
+  var copyText = document.getElementById(name);
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+}
