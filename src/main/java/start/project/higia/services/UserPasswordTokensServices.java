7e85a0f29a7f8bf4b4dc18c24112793ac6b927bf
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-08 15:06:09 -0300

    :sparkles: feat: implementing

---
+++ src/main/java/start/project/higia/services/UserPasswordTokensServices.java
@@ -0,0 +1,31 @@
+package start.project.higia.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import start.project.higia.models.User;
+import start.project.higia.models.UserPasswordTokens;
+import start.project.higia.repositories.UserPasswordTokensRepository;
+import start.project.higia.repositories.UserRepository;
+
+public class UserPasswordTokensServices {
+
+
+	@Autowired
+	UserPasswordTokensRepository userPasswordTokensRepository;
+	
+	@Autowired
+	UserRepository userRepositoy;
+	
+	private static final int passwordComplexity = 10;
+	
+	public void create(final User user, final String token) {
+        final UserPasswordTokens myToken = new UserPasswordTokens(token, user);
+        userPasswordTokensRepository.save(myToken);
+    }
+	
+
+	
+}
