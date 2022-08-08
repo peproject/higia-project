b22b9f6e398299f515db8a0615f5e9e198cc5f6d
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-08 15:24:36 -0300

    :sparkles: feat: Email search query by user

--- src/main/java/start/project/higia/repositories/UserRepository.java
+++ src/main/java/start/project/higia/repositories/UserRepository.java
@@ -3,12 +3,17 @@ package start.project.higia.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import start.project.higia.models.User;
 
 @Component
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query("SELECT u From User u WHERE u.email LIKE :email")
+	User findEmail(@Param("email") String  email);
 	
 	public Optional<User> findByEmail(String email);
 }
