0c63064a6b8ed6d195ce0e91aa1ffc96ed849c7d
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-09 14:37:44 -0300

    :bug: fix: redirect to accept cookies

--- src/main/java/start/project/higia/controllers/CookiesController.java
+++ src/main/java/start/project/higia/controllers/CookiesController.java
@@ -3,10 +3,19 @@ package start.project.higia.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CookiesController {
+
+	@GetMapping("/cookies")
+	public String LogoutCookieAccept(HttpSession session) {
+
+		session.setAttribute("cookies", 1);
+
+		return "redirect:/";
+	}
 
 	@PostMapping("/cookies")
 	public String acceptCookies(HttpSession session) {
