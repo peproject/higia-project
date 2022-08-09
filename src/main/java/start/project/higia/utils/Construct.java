459687667a8cafd59ebef3fc3af6b3167c96f186
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-09 14:39:31 -0300

    :bug: fix: url

--- src/main/java/start/project/higia/utils/Construct.java
+++ src/main/java/start/project/higia/utils/Construct.java
@@ -19,12 +19,12 @@ public class Construct {
 	}
 
 	public SimpleMailMessage ResetTokenEmail(String contextPath, Locale locale, String token, User user) {
-		String url = contextPath + "/doctor/change?token=" + user;
+		String url = contextPath + token;
 		return constructEmail(
 			"Recuperar senha",
 			"clique <a href=\"" + url + "\" targe=\"_blank\">aqui</a> para alterar sua senha!", user.getEmail());
 	}
-	
+
 	private SimpleMailMessage constructEmail(String subject, String body, String email) {
 		SimpleMailMessage mail = new SimpleMailMessage();
 		mail.setSubject(subject);
@@ -33,7 +33,7 @@ public class Construct {
 		mail.setFrom("Higia <starthigiaproject@gmail.com>");
 		return mail;
 	}
-	
-	
+
+
 
 }
