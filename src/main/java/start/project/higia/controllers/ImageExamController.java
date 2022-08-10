b9613a8282a0234e6aaa5e7b973c390a3e4dda2d
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-10 17:27:20 -0300

    :recycle: refactor: view and upload image

--- src/main/java/start/project/higia/controllers/ImageExamController.java
+++ src/main/java/start/project/higia/controllers/ImageExamController.java
@@ -4,37 +4,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import start.project.higia.models.ImageExam;
-import start.project.higia.services.ImageExamService;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Optional;
+import start.project.higia.models.ImageExam;
+import start.project.higia.models.User;
+import start.project.higia.services.ImageExamService;
+import start.project.higia.services.S3Service;
+import start.project.higia.services.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class ImageExamController {
 
 	@Autowired
-	private ImageExamService imageExamService;
+	private ImageExamService service;
 
-	@GetMapping("/imageExam")
-	public String uploadPhoto() {
-		return "imageExam";
+	@Autowired
+	private S3Service s3;
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/doc/list/images")
+	public String imageList(Model model) {
+		model.addAttribute("images", service.findAllImageExams());
+
+		return "exams/image";
 	}
 
-	@GetMapping("/imageExam/{id}")
-	public String getById(@PathVariable Long id, Model model) {
-		Optional<ImageExam> imageExamOptional = imageExamService.getById(id);
-		model.addAttribute("imageExam", imageExamOptional);
-		return "get/imageExam";
+	@PostMapping("/image/save")
+	public String save(@Valid ImageExam image, String cpf, @RequestParam(value = "file") MultipartFile file,
+		RedirectAttributes redirect) {
+
+		try {
+			User user = userService.findByCpf(cpf);
+
+
+			image.setUser(user);
+			image.setName(s3.uploadFile(file));
+			service.create(image);
+
+			redirect.addFlashAttribute("message", "Exame salvo");
+			redirect.addFlashAttribute("style", "p-3 mb-2 bg-success text-white rounded");
+			redirect.addFlashAttribute("icon", "fa-solid fa-check");
+
+			return "redirect:/test";
+		} catch(NegativeArraySizeException err) {
+
+				redirect.addFlashAttribute("message", "Não existe um paciente com esse CPF");
+				redirect.addFlashAttribute("style", "p-3 mb-2 bg-danger text-white rounded");
+				redirect.addFlashAttribute("icon", "fa-solid fa-triangle-exclamation");
+
+				return "redirect:/test";
+		}
 	}
-
-	@GetMapping("/imageExam/delete/{id}")
-	public String deleteImageExam(@PathVariable Long id) {
-		imageExamService.deleteById(id);
-		return "redirect:/user/login";
-	}
-
-
 
 }
