269727835bc3da8eb08a49c8cf1e1cd2c387e9da
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-10 17:26:59 -0300

    :recycle: refactor: exams

--- src/main/java/start/project/higia/controllers/ExamController.java
+++ src/main/java/start/project/higia/controllers/ExamController.java
@@ -1,30 +1,15 @@
 package start.project.higia.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import start.project.higia.models.Exam;
-import start.project.higia.services.ExamService;
 
 @Controller
 public class ExamController {
 
-	@Autowired
-	private ExamService examService;
+	@GetMapping("/exams")
+	public String exams() {
 
-	@PostMapping("/create/exam")
-	public String create(Exam exam) {
-		examService.create(exam);
-		return "";
-	}
-
-	@GetMapping("/exam/{id}")
-	public String getExamsById(@PathVariable Long id, Model model) {
-		model.addAttribute("exams", examService.getById(id));
-		return "";
+		return "exams/index";
 	}
 
 }
