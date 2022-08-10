62012356bddc9452a16a3552b5e9a65232cc0b47
Gabriel Mariz <gabriellucena997@outlook.com>
2022-08-10 17:28:11 -0300

    :recycle: refactor: view and save db

--- src/main/java/start/project/higia/controllers/exams/UrineController.java
+++ src/main/java/start/project/higia/controllers/exams/UrineController.java
@@ -1,48 +1,59 @@
 package start.project.higia.controllers.exams;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import start.project.higia.models.User;
 import start.project.higia.models.exams.Urine;
 import start.project.higia.services.UserService;
 import start.project.higia.services.exams.UrineService;
 
 @Controller
 public class UrineController {
-	
+
 	@Autowired
 	private UrineService service;
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@GetMapping("/urine/create")
-	public String create(Urine urine) {
 
-		return "";
-	}
-	
-	@PostMapping("/urine/save")
-	public String save(Urine urine) {
-		service.create(urine);
-		return "";
-	}
-	
-	@GetMapping("/urine/index/{id}")
-	public String indexById(@PathVariable Long id, Model model) {
-		model.addAttribute("urines", service.index(id));
-		return "tests/zap";
-	}
-	
 	@GetMapping("/doc/list/urines")
 	public String urineList(Urine urine, Model model) {
-		model.addAttribute("urines", service.indexAll(urine));
+
+		model.addAttribute("urine", service.indexAll(urine));
+
 		System.out.println(service.indexAll(urine));
-		return "tests/zap";
-	
+		return "exams/urine";
+
+	}
+
+	@PostMapping("/urine/save")
+	public String save(@Valid Urine urine, String cpf, RedirectAttributes redirect) {
+
+		try {
+			User user = userService.findByCpf(cpf);
+
+			System.out.println(cpf);
+			urine.setUser(user);
+			service.create(urine);
+
+			redirect.addFlashAttribute("message", "Exame salvo");
+			redirect.addFlashAttribute("style", "p-3 mb-2 bg-success text-white rounded");
+			redirect.addFlashAttribute("icon", "fa-solid fa-check");
+
+			return "redirect:/doc/list/urines";
+		} catch(NullPointerException err) {
+
+				redirect.addFlashAttribute("message", "Não existe um paciente com esse CPF");
+				redirect.addFlashAttribute("style", "p-3 mb-2 bg-danger text-white rounded");
+				redirect.addFlashAttribute("icon", "fa-solid fa-triangle-exclamation");
+
+				return "redirect:/doc/list/urines";
+		}
 	}
 }
