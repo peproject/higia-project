af16053c7dc3bcacb0988bc0b0360955b72b923d
Guilherme81 <alanguilherme0707@gmail.com>
2022-08-10 10:59:26 -0300

    :sparkles: feat: doctor route redirect

---
+++ src/main/java/start/project/higia/middleware/DoctorLogged.java
@@ -0,0 +1,38 @@
+package start.project.higia.middleware;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class DoctorLogged implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if (request.getSession().getAttribute("doctor") != null) {
+			request.getRequestDispatcher("/doc").forward(request, response);
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+	       HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(
+	  HttpServletRequest request,
+	  HttpServletResponse response,
+	  Object handler, Exception ex) throws Exception {
+	   HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+	
+}
